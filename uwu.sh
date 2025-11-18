4f52dbbdb3b034d675a38a6ef09cf905144f545c
Ivory Hunt <162629748+Ivory-Hnt@users.noreply.github.com>
2025-11-18 19:50:16 +0000

    Enhance uwu.sh to install programs and configure shell
    This script installs various programs, switches the shell to Zsh, installs Yay, and enables the graphical environment.

--- uwu.sh
+++ uwu.sh
@@ -1,10 +1,38 @@
 #/bin/bash
 
+echo ""
+echo "Installing programs"
+echo ""
+
 sudo pacman -S lxqt kitty zsh fastfetch hyfetch base-devel linux-zen-headers git chromium hyprland 
 sudo pacman -S --needed xorg gdm plasma kde-applications
 sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings
+echo ""
+echo "Programs installed sucessfully"
+echo ""
+
+echo ""
+echo "Switching shell"
+echo ""
+
 sudo chsh -s /bin/zsh
 
+echo ""
+echo "Shell switched sucessfully"
+echo ""
+
+echo ""
+echo "Installing Yay"
+echo ""
+
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+echo ""
+echo "Yay installed sucessfully"
+echo ""
+
+echo ""
+echo "Enabling the graphical environment"
+echo ""
 
 sudo systemctl enable --now gdm
